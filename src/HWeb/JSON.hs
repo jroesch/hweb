107529896fb5fd11406e5c9f09f1dba24de7c7fe
Jared Roesch <roeschinc@gmail.com>
2014-02-27 18:01:59 -0800

    update after day 2 of in-class

--- src/HWeb/JSON.hs
+++ src/HWeb/JSON.hs
@@ -21,17 +21,28 @@ type Array = V.Vector Value
 
 data Number = I !Integer 
             | D !Double
+            deriving (Eq, Show)
 
 json :: Parser Value
 json = (Object <$> object) 
      <|> (Array <$> array)
 
+-- Focus here
 value :: Parser Value
 value =  (Object <$> object)
      <|> (Array <$> array)
      <|> (String <$> str)
      <|> (Number <$> number)
+     <|> (Bool   <$> bool)
      <|> (const Null <$> nullP)
+
+instance Show Value where
+    show (Object obj) = show obj
+    show (Array arr)  = show arr
+    show (String str) = show str
+    show (Number num) = show num
+    show (Bool b)     = show b
+    show Null         = "null"
 
 object :: Parser Object
 object = do
@@ -54,8 +65,22 @@ array = do
     return $ V.fromList vs
   where sep = spaces >> char ',' >> spaces
 
-number = undefined
-str = undefined
+number :: Parser Number
+number = do
+    num <- string "0" <|> nonzero
+    return $ I (read num)
+  where nonzero = do
+          d  <- oneOf ['1'..'9']
+          ds <- many digit
+          return $ d:ds
 
--- javascript shit right hurr #YOLO
-nullP = undefined
+str :: Parser T.Text
+str = fmap T.pack (inQuotes $ many $ noneOf "\"")
+  where inQuotes = between (char '"') (char '"')
+
+nullP :: Parser String
+nullP = string "null"
+
+bool = trueP <|> falseP
+  where trueP = const True <$> string "true"
+        falseP = const False <$> string "false"
--- src/Web.hs
+++ src/Web.hs
@@ -2,13 +2,15 @@
 module Main where
 
 import Network.Wai (Application, Response, responseBuilder)
-import Network.Wai.Handler.Warp 
+import Network.Wai.Handler.Warp -- exports "run"
 import Network.HTTP.Types.Status
 -- Construct a Builder from a String
-import Blaze.ByteString.Builder.Char.Utf8 (fromString)
+import Blaze.ByteString.Builder.Char.Utf8 (fromString) -- import builder
 
+import HWeb.JSON
 -- makeLenses ''W.Request
 --
+
 
 simpleStringResponse :: String -> Response
 simpleStringResponse s = responseBuilder status200 [] (fromString s)
@@ -19,4 +21,4 @@ main = do
     run 9000 application
 
 application :: Application
-application request = return $ simpleStringResponse "Hello World!"
+application _ = return $ simpleStringResponse "Hello Class!"
